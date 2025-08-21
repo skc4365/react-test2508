db40a3bcd9c8515d300001308f817465c4066a97
skc4365 <50658047+skc4365@users.noreply.github.com>
2025-08-21 10:03:41 +0900

    Updates

--- src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-
-export default function Home() {
-    return (
-        <div style={{ padding: "40px" }}>
-            <h1>홈페이지 메인</h1>
-            <p>여기는 메인 페이지입니다.</p>
-        </div>
-    );
-}
