f0ea8e15a75ac8953a1976199a8a0d071579c015
skc4365 <50658047+skc4365@users.noreply.github.com>
2025-08-21 09:58:31 +0900

    edit config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: mode === 'production'  ?  '/react-test2508/'  :  '/',  
   plugins: [react()],
 })
